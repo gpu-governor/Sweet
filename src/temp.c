1c0b91154c360f4c18603c52edfb45b23d7379e1
cpu governor <cpu.governor@gmail.com>
2024-10-06 09:51:13 +0100

    updated temp.c

--- src/temp.c
+++ src/temp.c
@@ -10,21 +10,21 @@
 #include"widgets.h"
 
 int main() {
-   sw_init("Gui", 500, 800);
+   sw_init("Gui", 680, 680);
    
    //buttons
-   CREATE my_button = sw_button("click me", 200, 150, h4, WHITE, BLACK, BOLD);  
-   CREATE my_button1 = sw_button("click me", 200, 250, h4, WHITE, BLUE, BOLD);
-   CREATE my_button2 = sw_button("click me", 200, 350, h4, WHITE, GREEN, BOLD);   
-   CREATE my_button3 = sw_button("light", 200, 450, h4, BLACK, YELLOW, BOLD);
+   CREATE my_button = sw_button("click me", 200, 100, h4, WHITE, BLACK, BOLD);  
+   CREATE my_button1 = sw_button("click me", 200, 200, h4, WHITE, BLUE, BOLD);
+   CREATE my_button2 = sw_button("click me", 200, 300, h4, WHITE, GREEN, BOLD);   
+   CREATE my_button3 = sw_button("light", 200, 400, h4, BLACK, YELLOW, BOLD);
    
    // text based
-   CREATE my_label = sw_label("This is a Label", set, set, h1, BLACK, BLUE, BOLD);
-   CREATE my_text = sw_text("haha hi, you've been on my mind", 2, 0, h3, WHITE,BOLD);
+   CREATE my_label = sw_label("This is a Label", 0, 30, h1, BLACK, BLUE, BOLD);
+   CREATE my_text = sw_text("haha hi, you've been on my mind", 0, 2, h3, WHITE,BOLD);
    
    
     const char* options[] = {"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"};
-    CREATE dropdown = sw_drop_down(220, 550, options, 5);
+    CREATE dropdown = sw_drop_down(450, 100, options, 5);
 
    // writing this because i may forget, if a widgets is not showing up ensure it is registering
    
